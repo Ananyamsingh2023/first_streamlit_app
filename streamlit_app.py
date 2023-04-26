aa3bee8db8b56ed94e242591fccd780d334b36be
Ananyamsingh2023 <131661928+Ananyamsingh2023@users.noreply.github.com>
2023-04-26 13:32:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #dont run anything past this
-streamlit.stop()
+#streamlit.stop()
 
 
 
